1326dd37efb5aa23b31bc24e551a46970fb410de
Thiago de Melo Mota <82675235+Ghitado@users.noreply.github.com>
2024-05-14 21:09:32 -0300

    fix: Ajuste lógica SPI

--- src/app/components/matriz-tres-por-tres/matriz-tres-por-tres.component.ts
+++ src/app/components/matriz-tres-por-tres/matriz-tres-por-tres.component.ts
@@ -122,7 +122,7 @@ export class MatrizTresPorTresComponent implements OnInit {
 
     if (this.detA !== 0) {
       this.classification = "Sistema determinado";
-    } else if ((this.detA) && ((this.detX == 0) || (this.detY == 0) || (this.detZ == 0)) {
+    } else if ((this.detA == 0) && ((this.detX == 0) || (this.detY == 0) || (this.detZ == 0)) {
       this.classification = "Sistema Possivel e Indeterminado";
     } else {
       this.classification = "Sistema Impossivel";

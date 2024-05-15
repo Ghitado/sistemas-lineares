7895d7d8b50cd8854443c78ec3c487a04222e7ef
Thiago de Melo Mota <82675235+Ghitado@users.noreply.github.com>
2024-05-14 21:21:01 -0300

    chore: Retirando redundância

--- src/app/components/matriz-tres-por-tres/matriz-tres-por-tres.component.ts
+++ src/app/components/matriz-tres-por-tres/matriz-tres-por-tres.component.ts
@@ -122,7 +122,7 @@ export class MatrizTresPorTresComponent implements OnInit {
 
     if (this.detA !== 0) {
       this.classification = "Sistema determinado";
-    } else if ((this.deta == 0) && ((this.detx == 0) || (this.dety == 0) || (this.detz == 0)) {
+    } else if ((this.detX == 0) || (this.detY == 0) || (this.detZ == 0)) {
       this.classification = "Sistema Possivel e Indeterminado";
     } else {
       this.classification = "Sistema Impossivel";
